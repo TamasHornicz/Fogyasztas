0f8c0e31c280b44ee5af7552ed3177a43947ed72
Hornicz Tamás Csaba <horniczt.9d@acsj.net>
2022-04-06 14:20:51 +0200

    fogyasztás

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function szamol () {
     let V = document.getElementById('tank').value;
     let Ut = document.getElementById('ut').value;
 
-    let megtehetoUt = V * 100/F;
+    let megtehetoUt = V * 100 / F;
 
 
     if (megtehetoUt < Ut) {
